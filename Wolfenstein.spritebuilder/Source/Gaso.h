a37f4a30b75f3e1759bfb692908a366971110748
sreejitr <sreejitr@andrew.cmu.edu>
2015-04-16 17:09:08 -0700

    Added new enemy - Gaso, added animations for Idle, hit, punch, walk in Spritebuilder, added Gaso.m and Gaso.h classes in xcode, added animation manager to access animations in code

---
+++ Wolfenstein.spritebuilder/Source/Gaso.h
@@ -0,0 +1,16 @@
+//
+//  Gaso.h
+//  Wolfenstein
+//
+//  Created by Sreejita Ray on 4/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gaso : CCSprite
+- (void) hit;
+- (void) idle;
+- (void) punch;
+- (void) run;
+@end
---
+++ Wolfenstein.spritebuilder/Source/Gaso.m
@@ -0,0 +1,45 @@
+//
+//  Gaso.m
+//  Wolfenstein
+//
+//  Created by Sreejita Ray on 4/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gaso.h"
+
+@implementation Gaso {
+    CCAnimationManager* animationManager;
+}
+
+- (void) didLoadFromCCB {
+    animationManager = self.animationManager;
+    self.physicsBody.collisionType = @"enemy";
+}
+
+- (void) idle {
+    [animationManager runAnimationsForSequenceNamed:@"Idle"];
+}
+
+- (void) punch {
+//    if (self.flipX == NO) {
+//        id moveBy = [CCActionMoveTo actionWithDuration:0.30 position:ccp(self.position.x - 40, self.position.y)];
+//        [self runAction:moveBy];
+//    }
+    [animationManager runAnimationsForSequenceNamed:@"Punch"];
+}
+
+- (void) run {
+    [animationManager runAnimationsForSequenceNamed:@"Walk"];
+}
+
+- (void) hit {
+    if (self.flipX == YES) {
+        id moveBy = [CCActionMoveTo actionWithDuration:0.30 position:ccp(self.position.x - 40, self.position.y)];
+        [self runAction:moveBy];
+    }
+    [animationManager runAnimationsForSequenceNamed:@"Hit"];
+    [self performSelector:@selector(idle) withObject:nil afterDelay:1.8f];
+}
+
+@end
